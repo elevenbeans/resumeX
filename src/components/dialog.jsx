dff9175a4b48b9a5aab292bb9f915c0270695e5b
elevenBeans <elevenbeansf2e@gmail.com>
2017-11-05 06:49:13 +0800

    finish UI

--- src/components/dialog.jsx
+++ src/components/dialog.jsx
@@ -22,8 +22,22 @@ const Dialog = () => (
 				<Loading />
 			</div>
 		</div>
-				<div className="msg-in-right">
-			<div className = "item" ><Loading /></div>
+		<div className="msg-in-left">
+			<div className = "item" >
+				于是史上最贵iPhone X一出，常年吐槽iPhone的各位就突然嘴软了。
+				今天，我在美国硅谷的一家百思买去提取新手机时，就收获了一大筐iPhone X新用户对自己的吐槽：不是它难用，肯定是我笨！
+				这个系列就这样轰轰烈烈地在人群中传唤开来。
+			</div>
+		</div>
+		<div className="msg-in-right">
+			<div className = "item" >
+				使用iPhone X是需要智商的。像我这种时不时智商欠费的果粉可能并没有什么资格使用新款iPhone X手机。
+			</div>
+		</div>
+		<div className="msg-in-right">
+			<div className = "item" >
+				总之，这一天，我对自己的智商有了重新的认识——欠费！
+			</div>
 		</div>
 	</div>
 )
--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,86 @@
 'use strict';
 
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import Dialog from './components/dialog';
+import AppHeader from './components/header';
 
 import './style.less';
 
 // 关闭 HMR 调整 CSS，开启调整 JS
 // if (module.hot) module.hot.accept();
-
-const App = () => (
-    <div className = "app-wrapper">
-    	<div className = "app-phone" >
-    		<div className = "app-message">
-    			<Dialog />
-    		</div>
-    	</div>
-    </div>
-);
+class App extends Component {
+	constructor(props) {
+    super(props);
+    this.state = {
+    	inputPannelOn: false
+    };
+  }
+  showInputPannel = () => {
+  	console.log('sdf');
+  	this.setState({
+  		inputPannelOn: true
+  	});
+  }
+  closeInputPannel =() =>{
+	  this.setState({
+			inputPannelOn: false
+		});
+  }
+	render() {
+		return (
+			<div className = "app-wrapper">
+	    	<AppHeader></AppHeader>
+	    	<div className = "app-phone" >
+	    		<div className = "phone-wrapper">
+	    			{this.state.inputPannelOn &&
+		    				<div className = "app-mask"></div>
+		    		}
+	    			{this.state.inputPannelOn &&
+	    				<div
+	    					className = "app-input-pannel"
+	    					onClick = {this.closeInputPannel}
+	    				>
+		    				<div
+			    				className = "app-input-bar"
+			    			>
+			    				请输入 ...
+			    				<div className  = "app-input-button">
+			    					<div className = "input-arrow"></div>
+			    				</div>
+			    			</div>
+			    			<div className = "app-input-options">
+			    				<ul>
+			    					<li>是的</li>
+			    					<li>不对</li>
+			    					<li>哈哈哈</li>
+			    				</ul>
+			    			</div>
+	    				</div>
+	    			}
+	    			<div className = "app-message">
+		    			<div className = "message-padding"></div>
+		    			<div className = "message-content">
+		    				<Dialog />
+		    			</div>
+		    		</div>
+	    			{!this.state.inputPannelOn &&
+	    				<div
+		    				className = "app-input-bar"
+		    				onClick = {this.showInputPannel}
+		    			>
+		    				请输入 ...
+		    				<div className  = "app-input-button">
+		    					<div className = "input-arrow"></div>
+		    				</div>
+		    			</div>
+	    			}
+	    		</div>
+	    	</div>
+	    </div>
+		)
+	}
+}
 
 // 最终渲染
 ReactDom.render((
